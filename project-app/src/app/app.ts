7aa484121add6885b7df141f76ec389a1a1d18ea
Jasonbayford97 <jasonbayford97@gmail.com>
2026-02-14 15:39:49 +0200

    Add color scheme UI and theme styles
    Introduce a visual color-scheme test layout and theme variables. Replaced the simple message view with a set of color swatches and Bootstrap component examples in app.html. Added CSS custom properties and SCSS theme variables plus utility classes (text/bg/border-rust) and a page background gradient in styles.scss. Applied a .todo-app class to the body in index.html to enable the new background, and removed the unused signal import and message signal from app.ts.

--- project-app/src/app/app.ts
+++ project-app/src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,4 @@ import {Component, signal} from '@angular/core';
   styleUrl: './app.scss'
 })
 export class App {
-
-  protected readonly message = signal("Todo rust project");
 }
